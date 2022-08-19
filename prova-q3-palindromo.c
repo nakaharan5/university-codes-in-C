89ed92ab5de5c246ae3c84e1354b528a04460843
Thiago Frank <90275895+nakaharan5@users.noreply.github.com>
2022-08-19 00:16:09 -0300

    Create prova-q3-palindromo.c

---
+++ prova-q3-palindromo.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(){
+	int i, b=-1;
+	char string[100], auxiliar[100];
+	
+	printf("Digite um nome: \n");
+	gets(string);
+	
+	for(i=0;string[i]!='\0';i++){
+		b++;
+	}
+	for(i=0;string[b]!='\0'; i++){
+		auxiliar[b]=string[i];
+		b--;
+	}
+	
+	if(string[i]==auxiliar[i]){
+		printf("eh um palindromo");
+	} else {
+		printf("Nao eh um palindromo");
+	}
+}
